96327962437f3780784857671b64744fd2bd7911
gitchandler1 <chandlerbaggett@pm.me>
2023-08-21 16:52:37 -0600

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,8 @@ package org.example;
 
 import java.util.Random;
 import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Set<Integer> set = new Random().ints(1, 31)
